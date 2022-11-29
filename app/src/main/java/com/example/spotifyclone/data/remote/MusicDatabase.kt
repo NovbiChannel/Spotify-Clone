b5d87f7b285622fabcb8845bbf4fd9b062a20c60
NovbiChannel <chaglovnikita@gmail.com>
2022-11-29 13:35:19 +0500

    Setting up data storage. Firebase database setup.

---
+++ app/src/main/java/com/example/spotifyclone/data/remote/MusicDatabase.kt
@@ -0,0 +1,20 @@
+package com.example.spotifyclone.data.remote
+
+import com.example.spotifyclone.data.entities.Song
+import com.example.spotifyclone.other.Constants.SONG_COLLECTION
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+
+class MusicDatabase {
+
+    private val firestore = FirebaseFirestore.getInstance()
+    private val songCollection = firestore.collection(SONG_COLLECTION)
+
+    suspend fun getAllSongs(): List<Song> {
+        return try {
+            songCollection.get().await().toObjects(Song::class.java)
+        } catch (e: Exception) {
+            emptyList()
+        }
+    }
+}
