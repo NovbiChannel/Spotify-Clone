aa8d4b5cc6e51f686eca99c30ba937341dc3012c
NovbiChannel <chaglovnikita@gmail.com>
2022-11-30 14:24:15 +0500

    Write the functionality to show the music notification to pause/resume and skip songs.

--- app/src/main/java/com/example/spotifyclone/other/Constants.kt
+++ app/src/main/java/com/example/spotifyclone/other/Constants.kt
@@ -3,4 +3,7 @@ package com.example.spotifyclone.other
 object Constants {
 
     const val SONG_COLLECTION = "songs"
+
+    const val NOTIFICATION_CHANNEL_ID = "music"
+    const val NOTIFICATION_ID = 1
 }
