d3f8a91df5c1a84858fd215e47fd720587dc05a3
NovbiChannel <chaglovnikita@gmail.com>
2022-12-02 13:48:14 +0500

    Created a BaseSongAdapter to reduce boilerplate code for two adapters that me need in my project.

--- app/src/main/java/com/example/spotifyclone/adapters/SwipeSongAdapter.kt
+++ app/src/main/java/com/example/spotifyclone/adapters/SwipeSongAdapter.kt
@@ -12,18 +12,15 @@ import com.example.spotifyclone.data.entities.Song
 import kotlinx.android.synthetic.main.list_item.view.*
 import javax.inject.Inject
 
-class SongAdapter @Inject constructor(
-    private val glide: RequestManager
-) : BaseSongAdapter(R.layout.list_item) {
+class SwipeSongAdapter : BaseSongAdapter(R.layout.list_item) {
 
     override val differ = AsyncListDiffer(this, diffCallback)
 
     override fun onBindViewHolder(holder: BaseSongAdapter.SongViewHolder, position: Int) {
         val song = songs[position]
         holder.itemView.apply {
-            tvPrimary.text = song.title
-            tvSecondary.text = song.subtitle
-            glide.load(song.imageUrl).into(ivItemImage)
+            val text = "${song.title} - ${song.subtitle}"
+            tvPrimary.text = text
 
             setOnClickListener {
                 onItemClickListener?.let { click ->
